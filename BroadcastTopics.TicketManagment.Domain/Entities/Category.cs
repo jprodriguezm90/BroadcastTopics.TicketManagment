ebcc5e6fab4073f99bc44808c9e459632ed050e4
Juan <jprodriguezm90@gmail.com>
2026-05-21 19:40:53 -0500

    Entities Domain

---
+++ BroadcastTopics.TicketManagment.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿
+using BroadcastTopics.TicketManagment.Domain.Common;
+
+namespace BroadcastTopics.TicketManagment.Domain.Entities
+{
+    public class Category: AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Event>? Events { get; set; }
+    }
+}
---
+++ BroadcastTopics.TicketManagment.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿
+using BroadcastTopics.TicketManagment.Domain.Common;
+
+namespace BroadcastTopics.TicketManagment.Domain.Entities
+{
+    public class Order : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
