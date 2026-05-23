90797f636752be4f0a9b5ab7a26d75cfde6ee4af
Juan <jprodriguezm90@gmail.com>
2026-05-22 21:04:32 -0500

    20260522 - Adding the Dependency Injection to AutoMapper and MediatR

--- BroadcastTopics.TicketManagment.Application/Profiles/MappingProfile.cs
+++ BroadcastTopics.TicketManagment.Application/Profiles/MappingProfile.cs
@@ -12,6 +12,8 @@ namespace BroadcastTopics.TicketManagement.Application.Profiles
         public MappingProfile()
         {
             CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
         }
     }
 }
